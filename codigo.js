205ef96960e92a07b8b3ac37fe6f8f35711ca737
Nicolas Origlia <noriglia@gmail.com>
2022-11-29 18:16:57 -0300

    numeros guardados

---
+++ codigo.js
@@ -0,0 +1,6 @@
+let alturaCm = 175;
+let alturaM = 1.75;
+let peso = 68.5;
+let pesoRoundAbajo = Math.floor(peso);
+let alturaRoundArriba = Math.ceil(alturaM);
+console.log(maxValue = Number.MAX_VALUE === Number.MAX_VALUE +1);
